ba12f4d8c5c6ca878ceca94cee72d03644b3918a
Ramzess1488 <83368937+Ramzess1488@users.noreply.github.com>
2021-06-27 21:20:55 +0300

    Update main.h

--- main.h
+++ main.h
@@ -14,6 +14,9 @@
 //------------------------------------------------------------------------------
 // дефайним порты
 //------------------------------------------------------------------------------
+//#define INVERT_MOD          // раскоментировать для инвертированого режима
+#define   NONINVERT_MOD       // раскоментировать для не инвертированого режима
+
 #define BUZERON        PORTD |= 0x20
 #define BUZEROFF       PORTD &= ~ 0x20
 #define BUZERINVERT    PORTD ^= 0x20
@@ -27,8 +30,14 @@
 #define Q2OFF          PORTD &= ~ 0x80
 #define Q3ON           PORTD |= 0x40
 #define Q3OFF          PORTD &= ~ 0x40
-#define GOOPWM         TCCR1A  |= 0x80
-#define STOPPWM        TCCR1A  &= ~ 0x80
+#ifdef NONINVERT_MOD
+#define GOOPWM         TCCR1A  |= 0x82
+#define STOPPWM        TCCR1A  &= ~ 0x82
+#endif
+#ifdef INVERT_MOD
+#define GOOPWM         TCCR1A  |= 0xC0
+#define STOPPWM        TCCR1A  &= ~ 0xC0
+#endif
 #define ODIN 1             
 #define PYAT 5
 //------------------------------------------------------------------------------
@@ -38,6 +47,7 @@ void init (void);
 void razbivaem_razryad (unsigned int vhod_chislo);
 void pik_pik (void);
 void piknut (int kolichestvo, int dlitelnost);
+void read_encoder (void);
 
 
 #endif /* MAIN_H_ */
